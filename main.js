e0470ccfadec5c1fb9579851a74de58a69062d59
Rodion Sibov <rodionsibov@gmail.com>
2021-07-01 12:33:32 +0300

    add alert if input value is empty

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const inputEl = document.querySelectorAll('textarea')[0]
 const outputEl = document.querySelectorAll('textarea')[1]
 
-function minifyHtml(type, input, output) {
+function minifyHtml(input, output) {
     output.value = input.value
         .replace(/\<\!--\s*?[^\s?\[][\s\S]*?--\>/g, '')
         .replace(/\>\s*\</g, '><');
 }
 
 document.querySelector("#htmlMinify").addEventListener("click", function () {
-    minifyHtml(this.innerHTML, inputEl, outputEl);
+    if(inputEl.value === '') alert('Paste or type your data please!')
+    minifyHtml(inputEl, outputEl);
     document.querySelector("#inputInfo").textContent = inputEl.value.length
     document.querySelector("#outputInfo").textContent = outputEl.value.length
     outputEl.addEventListener("focus", function () {
